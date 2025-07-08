70dda1115d1b8c9fe166617543bde6ff691cea6c
Abdellah <abdellahshafi7@gmail.com>
2025-07-08 07:23:22 +0300

    Create 2-transactional.py

---
+++ python-decorators-0x01/2-transactional.py
@@ -0,0 +1,25 @@
+import sqlite3 
+import functools
+
+def transactional(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        
+        try:
+            func(*args, **kwargs)
+            conn = args[0]
+            conn.commit()
+        except Exception as e:
+            print(f"An error occured {e}. Rolling back...")
+            conn.rollback()
+            
+        
+
+@with_db_connection 
+@transactional 
+def update_user_email(conn, user_id, new_email): 
+    cursor = conn.cursor() 
+    cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id)) 
+    #### Update user's email with automatic transaction handling 
+
+update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')
