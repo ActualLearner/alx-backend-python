b1ae2ecd014ead42c2eb35bd3b40d7f8e8f0add9
Abdellah <abdellahshafi7@gmail.com>
2025-07-20 19:48:23 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,37 +1,48 @@
-#!/usr/bin/env python3
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json
+
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
+
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Unit test for utils.py"""
-    
+    """Unit tests for utils.access_nested_map"""
+
     @parameterized.expand([
         ({'a': 1}, ("a",), 1),
         ({'a': {'b': 2}}, ('a', 'b'), 2),
         ({'a': {'b': 2}}, ('a', 'b'), 2)]
     )
-    def test_access_nested_map(self, nested_map, path, expected) -> bool:
+    def test_access_nested_map(self, nested_map, path, expected):
         """Test access_nested_map function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
-            
+
     @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map_exception(self, nested_map, path):
         """Test that KeyError is raised with the expected message."""
-        with self.assertRaises(KeyError) :
+        with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
-        
+
+
 class TestGetJson(unittest.TestCase):
     """Unit tests for get_json."""
+
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
     @patch('utils.requests.get')
-    def test_get_json(self, test_url: str, test_payload: dict, mock_get: Mock) -> None:
+    def test_get_json(self, test_url, test_payload, mock_get):
         """Test get_json"""
         mock_response = Mock()
         mock_response.json.return_value = test_payload
@@ -41,5 +52,3 @@ class TestGetJson(unittest.TestCase):
 
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
-
-
