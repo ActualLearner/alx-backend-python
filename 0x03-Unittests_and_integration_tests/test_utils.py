084595836fa2c348dda277223bd7e863a99e1a8d
Abdellah <abdellahshafi7@gmail.com>
2025-07-20 19:36:47 +0300

    feat: add a unit test for get_json function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -24,3 +24,22 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) :
             access_nested_map(nested_map, path)
         
+        
+class TestGetJson(unittest.TestCase):
+    """Unit tests for get_json."""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url: str, test_payload: dict, mock_get: Mock) -> None:
+        """Test get_json returns expected payload and calls requests.get once."""
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
