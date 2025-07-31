652d875686f83d65d52d1e77dccd26a0b15a1926
ActualLearner <abdellahshafi7@gmail.com>
2025-07-31 16:47:55 +0300

    feat: add RestrictAccessByTimeMiddleware middleware

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from django.http import JsonResponse
+import re
 
 
 class RequestLoggingMiddleware:
@@ -11,6 +13,30 @@ class RequestLoggingMiddleware:
         log_text = f"{datetime.now()} - User: {user} - Path: {request.path}\n"
         with open('./requests.log', 'a') as log:
             log.write(log_text)
+
+        response = self.get_response(request)
+        return response
+
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        path = request.path
+        hour = datetime.now().hour
+        restricted_hours = range(6, 9)
+
+        is_conversation_route = (
+            re.match(r"^/api/conversations", path) or
+            re.match(r"^/api/users/\d+/conversations", path)
+        )
+
+        if is_conversation_route and hour in restricted_hours:
+            return JsonResponse(
+                {"detail": "Messaging is disabled during restricted hours (1AM-6AM)."},
+                status=403
+            )
 
         response = self.get_response(request)
         return response
