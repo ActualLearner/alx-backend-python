39942eaa4d1849c5b17b0fd97e89ef2df7437f35
ActualLearner <abdellahshafi7@gmail.com>
2025-07-31 15:38:16 +0300

    chore: remove copied directory

--- 0x03-MessagingApp-Django/chats/auth.py
+++
@@ -1,9 +0,0 @@
-from rest_framework_simplejwt.authentication import JWTAuthentication
-
-
-class CustomJWTAuthentication(JWTAuthentication):
-    """
-    This can be extended in the future for additional custom auth logic.
-    For now, it's a direct subclass of JWTAuthentication.
-    """
-    pass
