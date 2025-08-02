d1912b2bda8efd1a23e9b131c40b8f0a040672a7
ActualLearner <abdellahshafi7@gmail.com>
2025-08-02 15:59:17 +0300

    feat: cache view for 1 minute

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import cache_page
 from messaging.models import Message, Notification
 
 # Create your views here.
@@ -16,6 +17,7 @@ def delete_user(self, request):
     return redirect('home')
 
 
+@cache_page(60) # Cache for 60 seconds
 def get_messages(self, request):
     sent_messages = Message.objects.filter(
         sender=request.user).select_related('parent_message')
