1af0a898f1329754186665e4742ceaede520a6ad
ActualLearner <abdellahshafi7@gmail.com>
2025-08-02 14:58:52 +0300

    feat: add a log_edit signal to track message edit history

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -3,17 +3,27 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class Messages(models.Model):
+class Message(models.Model):
     sender = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='sent_messages')
     receiver = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='recieved_messages')
     content = models.TextField()
+    edited = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.SET_NULL, null=True, related_name='message_history')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = (('message', 'timestamp'),)
 
 
 class Notification(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='notifications')
     message = models.ForeignKey(
-        Messages, on_delete=models.CASCADE, related_name='notifications')
+        Message, on_delete=models.CASCADE, related_name='notifications')
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,10 +1,17 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Messages, Notification
+from .models import Message, MessageHistory, Notification
 
 
-@receiver(post_save, sender=Messages)
+@receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
     """Signal to create notification for a user when they have recieve a message"""
     if created:
         Notification.objects.create(user=instance.receiver, message=instance)
+
+
+@receiver(pre_save, sender=Message)
+def log_edit(sender, instance, **kwargs):
+    """Signal to track message editing, log old message"""
+    instance.edited = True
+    MessageHistory.objects.create(message=instance, content=instance.content)
